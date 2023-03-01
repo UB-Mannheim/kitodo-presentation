284525c0c2dec5687b2eccb40ae22e44aad4df39
BFallert <bernd.fallert@uni-mannheim.de>
2023-03-01 14:54:28 +0100

    ListView: add called collection
    idea from @michaelkubina
    see: https://github.com/kitodo/kitodo-presentation/issues/918

--- Classes/Controller/ListViewController.php
+++ Classes/Controller/ListViewController.php
@@ -123,5 +123,6 @@ class ListViewController extends AbstractController
         $this->view->assign('lastSearch', $this->searchParams);
         $this->view->assign('sortableMetadata', $sortableMetadata);
         $this->view->assign('listedMetadata', $listedMetadata);
+        $this->view->assign('currentCollection', $collection);
     }
 }
