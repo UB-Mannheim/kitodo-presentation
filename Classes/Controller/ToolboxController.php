829433162ef1bf6fd7c82dc1637a0c86a9eaa0ec
Bernd Fallert <bernd.fallert@uni-mannheim.de>
2023-11-13 08:15:00 +0100

    Change and specify error messages in info

--- Classes/Controller/ToolboxController.php
+++ Classes/Controller/ToolboxController.php
@@ -323,7 +323,7 @@ class ToolboxController extends AbstractController
                 $file['url'] = $this->currentDocument->getDownloadLocation($fileId);
                 $file['mimetype'] = $this->currentDocument->getFileMimeType($fileId);
             } else {
-                $this->logger->warning('File not found in fileGrp "' . $fileGrp . '"');
+                $this->logger->warning('File not found in fileGrp "' . $fileGrp . '" (getImage) page: "' . $page . '"');
             }
         }
         return $file;
@@ -431,7 +431,7 @@ class ToolboxController extends AbstractController
             empty($firstPageLink)
             && empty($secondPageLink)
         ) {
-            $this->logger->warning('File not found in fileGrps "' . $this->extConf['files']['fileGrpDownload'] . '"');
+            $this->logger->notice('File not found in fileGrps "' . $this->extConf['files']['fileGrpDownload'] . '" (Page Download)');
         }
 
         if (!empty($firstPageLink)) {
@@ -469,7 +469,7 @@ class ToolboxController extends AbstractController
             }
         }
         if (empty($workLink)) {
-            $this->logger->warning('File not found in fileGrps "' . $this->extConf['files']['fileGrpDownload'] . '"');
+            $this->logger->notice('File not found in fileGrps "' . $this->extConf['files']['fileGrpDownload'] . '" (Work Download)');
         }
         return $workLink;
     }
