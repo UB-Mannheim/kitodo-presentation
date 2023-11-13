318131f2fc932b4ff2d476185679420944f9d4ae
Bernd Fallert <bernd.fallert@uni-mannheim.de>
2023-11-13 08:15:00 +0100

    Change and specify error messages in info

--- Classes/Controller/ToolboxController.php
+++ Classes/Controller/ToolboxController.php
@@ -267,7 +267,7 @@ class ToolboxController extends AbstractController
                 }
                 break;
             } else {
-                $this->logger->warning('File not found in fileGrp "' . $fileGrp . '"');
+                $this->logger->warning('File not found in fileGrp "' . $fileGrp . '" (getImage) page: "' . $page . '"');
             }
         }
         return $image;
@@ -351,7 +351,7 @@ class ToolboxController extends AbstractController
             empty($firstPageLink)
             && empty($secondPageLink)
         ) {
-            $this->logger->warning('File not found in fileGrps "' . $this->extConf['files']['fileGrpDownload'] . '"');
+            $this->logger->notice('File not found in fileGrps "' . $this->extConf['files']['fileGrpDownload'] . '" (Page Download)');
         }
 
         if (!empty($firstPageLink)) {
@@ -389,7 +389,7 @@ class ToolboxController extends AbstractController
             }
         }
         if (empty($workLink)) {
-            $this->logger->warning('File not found in fileGrps "' . $this->extConf['files']['fileGrpDownload'] . '"');
+            $this->logger->notice('File not found in fileGrps "' . $this->extConf['files']['fileGrpDownload'] . '" (Work Download)');
         }
         return $workLink;
     }
