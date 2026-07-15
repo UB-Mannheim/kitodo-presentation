5f3e004bbde3e889a78119b75f29814a15561edc
Bernd Fallert <bernd.fallert@uni-mannheim.de>
2026-07-15 23:15:58 +0200

    fix AnchorDocumentID, fix findOneBy

--- Classes/Domain/Repository/DocumentRepository.php
+++ Classes/Domain/Repository/DocumentRepository.php
@@ -887,7 +887,7 @@ class DocumentRepository extends AbstractRepository
      */
     public function getOnlytPreviousDocumentUid($uid)
     {
-        $currentDocument = $this->findOneByUid($uid);
+        $currentDocument = $this->findOneBy(['uid' => $uid]);
 
         if ($currentDocument) {
             $currentVolume = '';
