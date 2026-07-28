27b822947866b3897e7c78e6f52968ac533036be
BFallert <bernd.fallert@bib.uni-mannheim.de>
2026-07-28 11:54:01 +0200

    fix download

--- Resources/Public/JavaScript/OpenLayers/get-ol.sh
+++ Resources/Public/JavaScript/OpenLayers/get-ol.sh
@@ -3,7 +3,22 @@
 VERSION="7.2.2"
 
 wget -O ol.zip "https://github.com/openlayers/openlayers/releases/download/v$VERSION/v$VERSION-package.zip"
-unzip -p ol.zip "v$VERSION-package/dist/ol.js" > openlayers.js
-unzip -p ol.zip "v$VERSION-package/ol.css" > openlayers.css
-unzip -p ol.zip "v$VERSION-package/ol.js.map" > openlayers.js.map
-rm ol.zip
+
+
+# Create temp dir and extract
+mkdir -p temp_ol
+cd temp_ol
+unzip ../ol.zip
+
+#unzip -p ol.zip "v$VERSION-package/dist/ol.js" > openlayers.js
+#unzip -p ol.zip "v$VERSION-package/ol.css" > openlayers.css
+#unzip -p ol.zip "v$VERSION-package/dist/ol.js.map" > openlayers.js.map
+
+# Copy files
+cp dist/ol.js ../openlayers.js
+cp dist/ol.js.map ../openlayers.js.map
+cp ol.css ../openlayers.css
+
+# Cleanup
+cd ..
+rm -rf temp_ol ol.zip
