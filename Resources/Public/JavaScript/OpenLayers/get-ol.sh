0656c0b57cd639b903c2e9df9b07ddd1d97093f4
BFallert <bernd.fallert@bib.uni-mannheim.de>
2026-07-28 10:42:43 +0200

    update and add map-file

--- Resources/Public/JavaScript/OpenLayers/get-ol.sh
+++ Resources/Public/JavaScript/OpenLayers/get-ol.sh
@@ -5,4 +5,5 @@ VERSION="7.2.2"
 wget -O ol.zip "https://github.com/openlayers/openlayers/releases/download/v$VERSION/v$VERSION-package.zip"
 unzip -p ol.zip "v$VERSION-package/dist/ol.js" > openlayers.js
 unzip -p ol.zip "v$VERSION-package/ol.css" > openlayers.css
+unzip -p ol.zip "v$VERSION-package/ol.js.map" > openlayers.js.map
 rm ol.zip
